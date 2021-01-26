7fd5630d2a4f0e8205ac0cf8a9fc6beae3cc6fa5
leticiassiqueira25 <leticiassiqueira25@gmail.com>
2021-01-26 13:47:42 -0300

    arquivos iniciais

---
+++ DetalhesMoedas/Classes/DetalhesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetalhesViewController.swift
+//  DetalhesMoedas
+//
+//  Created by Leticia Sousa Siqueira on 26/01/21.
+//
+
+import UIKit
+import CommonsService
+
+class DetalhesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
