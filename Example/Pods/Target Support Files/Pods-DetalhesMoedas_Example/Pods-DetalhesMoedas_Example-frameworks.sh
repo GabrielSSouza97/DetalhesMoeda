95cadd5081f7772cd43595e7a739b5285c151852
leticiassiqueira25 <leticiassiqueira25@gmail.com>
2021-01-26 13:23:58 -0300

    arquivos iniciais

--- Example/Pods/Target Support Files/Pods-DetalhesMoedas_Example/Pods-DetalhesMoedas_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-DetalhesMoedas_Example/Pods-DetalhesMoedas_Example-frameworks.sh
@@ -175,9 +175,11 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CommonsService/CommonsService.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/DetalhesMoedas/DetalhesMoedas.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CommonsService/CommonsService.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/DetalhesMoedas/DetalhesMoedas.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
