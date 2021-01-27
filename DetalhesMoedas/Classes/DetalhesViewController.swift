b38a1fcccd364ec4c1578da768dfed993d06d42d
Letycia <letycia.sobrinho@hotmail.com>
2021-01-27 11:29:17 -0300

    criacao da xib detalhes

--- DetalhesMoedas/Classes/DetalhesViewController.swift
+++ DetalhesMoedas/Classes/DetalhesViewController.swift
@@ -7,24 +7,52 @@
 
 import UIKit
 import CommonsService
-
-class DetalhesViewController: UIViewController {
-
-    override func viewDidLoad() {
+public class DetalhesViewController: UIViewController {
+    
+    //MARK: @IBOutlet
+    
+    @IBOutlet weak var siglaMoeda: UILabel!
+    
+    @IBOutlet weak var imagemMoeda: UIImageView!
+    
+    @IBOutlet weak var valorPrincipal: UILabel!
+    
+    
+    @IBOutlet weak var removerAdicionar: UILabel!
+    
+    
+    @IBOutlet weak var valorUltimaHora: UILabel!
+    
+    @IBOutlet weak var valorUltimoDia: UILabel!
+    
+    
+    @IBOutlet weak var valorUltimoMes: UILabel!
+    
+    // MARK : @IBAction
+    
+    @IBAction func voltar(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    public init() {
+        super.init(nibName: "DetalhesViewController", bundle: Bundle(for: DetalhesViewController.self))
+    }
+    
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    public override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        siglaMoeda.text = "SIGLA MOEDA"
+        valorPrincipal.text = "$35.0000.22"
+        valorUltimoDia.text = "$123.456.78"
+        valorUltimoMes.text = "$123.456.78"
+        valorUltimaHora.text = "$123.456.78"
+      // imagemMoeda.image = image
+        
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
