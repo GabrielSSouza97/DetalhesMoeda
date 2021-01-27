2b5d3abe2f3f0cb682a539cedb781440b7a9e374
GabrielSSouza97 <gabriel.s.souza9755@gmail.com>
2021-01-27 11:44:55 -0300

    primeiro commit favoritos

--- DetalhesMoedas/Classes/FavoritosViewController.swift
+++ DetalhesMoedas/Classes/FavoritosViewController.swift
@@ -8,23 +8,45 @@
 import UIKit
 import CommonsService
 
-class FavoritosViewController: UIViewController {
-
-    override func viewDidLoad() {
+public class FavoritosViewController: UIViewController {
+    
+    //@IBOutlet weak var siglaMoeda: UILabel!
+    @IBOutlet weak var collectionViewFavoritos: UICollectionView!
+    let celulaFavorito = "celulaFavorito"
+    
+    public init() {
+        super.init(nibName: "FavoritosViewController", bundle: Bundle(for: FavoritosViewController.self))
+    }
+    
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    public override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let nibCell = UINib(nibName: celulaFavorito, bundle: nil)
+        collectionViewFavoritos.register(nibCell, forCellWithReuseIdentifier: celulaFavorito)
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
+
+//extension FavoritosViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+//
+//    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return 5
+//    }
+//
+//    public func numberOfSectionsInCollectionView(in collectionView: UICollectionView) -> Int {
+//        return 1
+//    }
+//
+//    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let celula = collectionView.dequeueReusableCell(withReuseIdentifier: celulaFavorito, for: indexPath) as! FavoritosCollectionViewCell
+//        celula.labelNomeMoeda.text = "Nome da Moeda"
+//        celula.labelSiglaMoeda.text = "SGL"
+//        celula.labelValorMoeda.text = "R$"
+//
+//        return celula
+//    }
+//
+//}
--- Example/DetalhesMoedas/ViewController.swift
+++ Example/DetalhesMoedas/ViewController.swift
@@ -7,18 +7,19 @@
 //
 
 import UIKit
+import DetalhesMoedas
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 }
-
